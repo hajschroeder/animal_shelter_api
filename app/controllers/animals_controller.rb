8f0472832f23d794fe2a2d32d7efc72f719441dd
Hans Schroeder <hajschroeder@gmail.com>
2022-06-30 15:46:29 -0700

    add CRUD to animals controller

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,2 +1,37 @@
 class AnimalsContreoller < ApplicationController
-  
+
+  def index
+    @animals = Animal.all
+    json_response(@animals)
+  end
+
+  def show 
+    @animal = Animal.find(params[:id])
+    json_response(@animal)
+  end
+
+  def create 
+    @animal = Animal.create!(animal_params)
+    json_response(@animal. :created)
+  end
+
+  def update
+    @animal = Animal.find(params[:id])
+    if @animal.update!(animal_params)
+      render status: 200, json: {
+        message: "Animal is successfully in the shelter!"
+      }
+    end
+  end
+
+  def destroy
+    @animal = Animal.find(params[:id])
+    @animal.destroy
+  end
+
+  private 
+
+  def animal_params
+    params.permit(:name, :breed, :age)
+  end
+end
