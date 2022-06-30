c6b119d581375c817189f916e8f9f7e2790f2ba9
Hans Schroeder <hajschroeder@gmail.com>
2022-06-30 14:32:54 -0700

    create database and include columns

---
+++ db/migrate/20220630212642_create_animal.rb
@@ -0,0 +1,11 @@
+class CreateAnimal < ActiveRecord::Migration[5.2]
+  def change
+    create_table :animals do |t|
+      t.string :name
+      t.string :breed
+      t.integer :age 
+
+      t.timestamps
+    end
+  end
+end
