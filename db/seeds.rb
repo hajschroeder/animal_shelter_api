0cb56e9a729840fef74ef13e40e5cec457a1c64d
Hans Schroeder <hajschroeder@gmail.com>
2022-07-01 13:41:16 -0700

    update seed file to account for animal species

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,25 @@ class Seed
 
   def generate_animals
     50.times do |i|
-      animal = Animal.create!(
-        name: Faker::TvShows::TwinPeaks.character, 
-        breed: Faker::Books::Dune.title,
-        age: Faker::Number.between(from: 1, to: 100)
-      )
+      @cat_or_dog = ['Cat', 'Dog']
+      @species = @cat_or_dog.sample
+      if @species == 'Cat' 
+        animal = Animal.create!(
+          species: @species,
+          name: Faker::TvShows::TwinPeaks.character, 
+          breed: Faker::Books::Dune.title,
+          age: Faker::Number.between(from: 1, to: 100)
+        )
+      else 
+        animal = Animal.create!(
+          species: @species,
+          name: Faker::TvShows::HeyArnold.character,
+          breed: Faker::Games::Pokemon.name,
+          age: Faker::Number.between(from: 1, to: 100)
+        )
+      end
       
-      p "Animal #{i}: #{animal.name} is a #{animal.breed} and they are #{animal.age} years old"
+      p "Animal #{i}: #{animal.name} is a #{animal.species} and they are a #{animal.age} years old #{animal.breed}"
     end
   end
 end
